26537d16cce52482763162de66813a1ad567bf63
Pablo Turrero <pablo.turrero@the-cocktail.com>
2022-02-22 17:47:35 +0100

    Archivos que no deberían estar en esta rama

---
+++ web.js
@@ -0,0 +1,19 @@
+let divMascotas = document.getElementById("mostrable");
+let mascotasSi = document.getElementById("mascotas_si");
+let mascotasNo = document.getElementById("mascotas_no");
+let textoMascotas = document.getElementById("numero");
+
+let mostrar = function() {
+    if (mascotasSi.checked)
+        divMascotas.classList = "";
+    else
+        divMascotas.classList = "oculta";
+};
+
+let esCero = function() {
+    if (parseInt(textoMascotas.value) === 0 || textoMascotas.value === "") {
+        alert("Eso es que en realidad no tienes mascotas.");
+        textoMascotas.value = "";
+        mascotasNo.click();
+    }
+};
